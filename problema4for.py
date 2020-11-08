baf8598bb439e4c941224aac8bec3a6b8325cf25
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-11-08 17:12:09 +0200

    Update problema4for.py

--- problema4for.py
+++ problema4for.py
@@ -1,3 +1,4 @@
+#Utilizînd ciclul FOR elaboraţi un program care afişează toate numerele impare, pentru intervalul de la a la b (a și b se citesc de la tastatură).
 a=eval(input("Introduceti un numar: "))
 b=eval(input("Introduceti un numar: "))
 c=1
